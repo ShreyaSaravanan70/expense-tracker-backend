5647e2ffad8c4b99907046f62c69e9c769fce2f1
Shreya Saravanan <shreyasaravanan70@gmail.com>
2026-01-19 19:34:04 +0530

    Prepare backend for Render deployment

--- app.py
+++ app.py
@@ -70,5 +70,5 @@ def delete_expense(index):
 # ---------- START ----------
 
 if __name__ == "__main__":
-    print("Server started at http://localhost:8080")
-    app.run(port=8080, debug=True)
+    app.run(host="0.0.0.0", port=5000)
+
